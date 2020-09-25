986aade577faca2907dbe923d9b3e0b75220c2a9
DMaxter <daniel_matos@outlook.pt>
2020-09-25 22:59:59 +0100

    Base project files

---
+++ GStore/PCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
