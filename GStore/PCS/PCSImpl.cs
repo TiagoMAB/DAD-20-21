208687ace9cb573a14f6ddc255c04dbb4867b2b0
DMaxter <daniel_matos@outlook.pt>
2020-10-01 00:25:56 +0100

    PCS server and protobuffer minor fixes

---
+++ GStore/PCS/PCSImpl.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using GStore;
+using System;
+using System.Threading.Tasks;
+
+namespace PCS
+{
+    public class PCSImpl : GStore.PCS.PCSBase
+    {
+        public override Task<ClientResponse> Client(ClientRequest request, ServerCallContext context)
+        {
+            Console.WriteLine(String.Format("Request to launch client with script file '{0}'", request.Script));
+
+            return Task.FromResult(new ClientResponse());
+        }
+
+        public override Task<ServerResponse> Server(ServerRequest request, ServerCallContext context)
+        {
+            Console.WriteLine(String.Format("Request to launch server with id '{0}', min delay of {1}ms and max delay of {2}ms", request.Id, request.MinDelay, request.MaxDelay));
+
+            return Task.FromResult(new ServerResponse());
+        }
+    }
+}
--- GStore/PCS/Program.cs
+++ GStore/PCS/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 
 namespace PCS
 {
     class Program
     {
+        const int port = 10000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = new Server
+            {
+                Services = { GStore.PCS.BindService(new PCSImpl()) },
+                Ports    = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("PCS running on port " + port);
+            Console.WriteLine("Press any key to shutdown...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 }
