ecdeb015e459db7a8d827fa20fe04dc07c7b25dd
TiagoMAB <tiago.barroso1@hotmail.com>
2020-10-11 16:29:52 +0100

    starting implementation of handshake protocol for setting up servers

--- GStore/Server/Services/GStoreService.cs
+++ GStore/Server/Services/GStoreService.cs
@@ -1,26 +1,38 @@
-using System.Threading.Tasks;
-using Grpc.Core;
 using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GStore;
 
 namespace Server
 {
     public class GStoreService : GStore.GStore.GStoreBase
     {
 
-        public GStoreService()
+        public GStoreService(int id, string URL, string otherURL)
         {
+            GrpcChannel channel = GrpcChannel.ForAddress(URL);
+            var server = new GStore.GStore.GStoreClient(channel);
         }
 
-        public override Task<GStore.WriteReply> write(GStore.WriteRequest request, ServerCallContext context)
+        public override Task<GStore.HandshakeReply> handshake(HandshakeRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Handshake");
+            return Task.FromResult(new HandshakeReply());
+        }
+
+
+        public override Task<GStore.WriteReply> write(WriteRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Write");
+            return Task.FromResult(new WriteReply());
+        }
+
+        public override Task<GStore.ReadReply> read(ReadRequest request, ServerCallContext context)
         {
             Console.WriteLine("Read");
-            return Task.FromResult(new GStore.WriteReply());
-        }
-
-        public override Task<GStore.ReadReply> read(GStore.ReadRequest request, ServerCallContext context)
-        {
-            Console.WriteLine("Reply");
-            return Task.FromResult(new GStore.ReadReply());
+            return Task.FromResult(new ReadReply());
         }
     }
 }
