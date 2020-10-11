25a561169c8d58bd096dcc8de33b4c15052a379b
TiagoMAB <tiago.barroso1@hotmail.com>
2020-10-11 16:28:33 +0100

    reworked input arguments

--- GStore/Server/Program.cs
+++ GStore/Server/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using GStore;
 using Grpc.Core;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Server
 {
@@ -10,31 +8,39 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            string host = null, serverHost = null;
-            int port, serverPort;
+            string id = null, URL = null, otherURL = null, host = null;
+            Int64 min_delay = 0, max_delay = 0;
+            int port = 0;
 
-            if (args.Length != 2 && args.Length != 4) 
+            if (args.Length != 4 && args.Length != 5) 
             {
-                Console.WriteLine("Wrong format. Should be: host port [serverHost serverPort]\n serverHost and serverPort are optional arguments used to connect to a pre-existing network.");    
+                Console.WriteLine("Wrong format. Should be: id url min_delay max_delay [otherURL]\n otherURL is an optional argument used to connect to a pre-existing network.");    
             }
             
-            host = args[0];
-            port = int.Parse(args[1]);
+            id = args[0];
+            URL = args[1];
+            min_delay = Int64.Parse(args[2]);
+            max_delay = Int64.Parse(args[3]);
             
-            if (args.Length == 4) 
+            string[] details = URL.Split("//");
+            details = details[1].Split(':');
+
+            host = details[0];
+            port = int.Parse(details[1]);
+           
+            if (args.Length == 5) 
             {
-                serverHost = args[2];
-                serverPort = int.Parse(args[3]);
+                otherURL = args[4];
             }
 
             Grpc.Core.Server server = new Grpc.Core.Server
             {
-                Services = { GStore.GStore.BindService(new GStoreService()) },
+                Services = { GStore.GStore.BindService(new GStoreService(id, URL, otherURL))},
                 Ports = { new ServerPort(host, port, ServerCredentials.Insecure) }
             };
 
             server.Start();
-            
+
             Console.WriteLine("GStore server running on " + host + " listening on port " + port);
             Console.WriteLine("Press any key to stop the server...");
             Console.ReadKey();
