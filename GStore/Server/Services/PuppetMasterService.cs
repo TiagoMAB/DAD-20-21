b3941f32d503c196cfc3cae56baf18a8efc3f170
TiagoMAB <tiago.barroso1@hotmail.com>
2020-10-13 19:10:15 +0100

    Added missing prototype file

---
+++ GStore/Server/Services/PuppetMasterService.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GStore;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Server
+{
+    public class PuppetMasterService : PuppetMaster.PuppetMasterBase
+    {
+
+        private readonly GStoreService service;
+
+        public PuppetMasterService(GStoreService service)
+        {
+            this.service = service;
+        }
+
+        public override Task<StatusResponse> Status(StatusRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Status request received");
+
+            //TO DO: implement
+
+            return Task.FromResult(new StatusResponse());
+        }
+
+        public override Task<FreezeResponse> Freeze(FreezeRequest request, ServerCallContext context)
+        {
+            //TO DO: implement
+
+            return Task.FromResult(new FreezeResponse());
+        }
+
+        public override Task<ReplicationResponse> Replication(ReplicationRequest request, ServerCallContext context)
+        {
+            //TO DO: implement
+            return Task.FromResult(new ReplicationResponse());
+        }
+        public override Task<PartitionResponse> Partition(PartitionRequest request, ServerCallContext context)
+        {
+            //TO DO: implement
+            return Task.FromResult(new PartitionResponse());
+        }
+        public override Task<CrashResponse> Crash(CrashRequest request, ServerCallContext context)
+        {
+            //TO DO: implement
+            return Task.FromResult(new CrashResponse());
+        }
+        public override Task<UnfreezeResponse> Unfreeze(UnfreezeRequest request, ServerCallContext context)
+        {
+            //TO DO: implement
+            return Task.FromResult(new UnfreezeResponse());
+        }
+
+    }
+}
