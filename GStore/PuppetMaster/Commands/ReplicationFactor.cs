80a08d6acdbba1910423461220c96050d7bcfff9
DMaxter <daniel_matos@outlook.pt>
2020-11-02 02:16:01 +0000

    Implemented ReplicationFactor

--- GStore/PuppetMaster/Commands/ReplicationFactor.cs
+++ GStore/PuppetMaster/Commands/ReplicationFactor.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GStore;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PuppetMaster.Commands {
@@ -10,8 +14,49 @@ namespace PuppetMaster.Commands {
         }
 
         protected override async Task DoWork() {
-            // TODO: Implement????
-            await Task.Run(() => Log(String.Format("ReplicationFactor set to {0}", this.factor))).ConfigureAwait(false);
+            List<Task> tasks = new List<Task>();
+
+            List<KeyValuePair<string, string>> ids = ConnectionInfo.GetAll();
+
+            ReplicationRequest request = new ReplicationRequest { Factor = this.factor };
+
+            foreach(var pair in ids) {
+                tasks.Add(Task.Run(async () => { 
+                    GrpcChannel channel = GrpcChannel.ForAddress(pair.Value);
+
+                    GStore.PuppetMaster.PuppetMasterClient client = new GStore.PuppetMaster.PuppetMasterClient(channel);
+
+                    try {
+                        await client.ReplicationAsync(request);
+
+                        Log(String.Format("ReplicationFactor set to {0} on '{1}'", this.factor, pair.Key));
+                    } catch (RpcException e) {
+                        String command = String.Format("Set replication factor on '{0}'", pair.Key);
+
+                        switch (e.StatusCode) {
+                            case StatusCode.Aborted:
+                                Log(String.Format("ABORTED: {0}", command));
+                                break;
+                            case StatusCode.Cancelled:
+                                Log(String.Format("CANCELLED: {0}", command));
+                                break;
+                            case StatusCode.DeadlineExceeded:
+                                Log(String.Format("TIMEOUT: {0}", command));
+                                break;
+                            case StatusCode.Internal:
+                                Log(String.Format("INTERNAL ERROR: {0}", command));
+                                break;
+                            default:
+                                Log(String.Format("UNKNOWN ERROR: {0}", command));
+                                break;
+                        }
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+
+            Log(String.Format("ReplicationFactor set to {0} on all servers", this.factor));
         }
     }
 }
