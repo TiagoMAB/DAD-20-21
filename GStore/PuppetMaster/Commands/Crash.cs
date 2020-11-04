caa3bd8e47662a49049daba646c7fc452e265a16
TiagoMAB <tiago.barroso1@hotmail.com>
2020-11-04 03:14:53 +0000

    Fixed Crash command in PuppetMaster

--- GStore/PuppetMaster/Commands/Crash.cs
+++ GStore/PuppetMaster/Commands/Crash.cs
@@ -28,7 +28,8 @@ namespace PuppetMaster.Commands {
             try {
                 await client.CrashAsync(new CrashRequest { } );
 
-                Log(String.Format("ERROR: Server not crashed '{0}'", this.id));
+                Log(String.Format("Crashed server {0}", this.id));
+                this.form.RemoveServer(this.id);
             } catch (RpcException e) {
                 String command = String.Format("Crash server '{0}'", this.id);
 
@@ -43,9 +44,7 @@ namespace PuppetMaster.Commands {
                         Log(String.Format("TIMEOUT: {0}", command));
                         break;
                     case StatusCode.Internal:
-                        Log(String.Format("Crashed server {0}", this.id));
-
-                        this.form.RemoveServer(this.id);
+                        Log(String.Format("INTERNAL: {0}", command));
                         break;
                     default:
                         Log(String.Format("UNKNOWN ERROR: {0}", command));
